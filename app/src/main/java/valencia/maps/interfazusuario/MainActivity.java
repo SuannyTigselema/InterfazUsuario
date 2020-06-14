ef1b62118272077a7397cd2512883d40cb8e552a
Suanny Tigselema <suannytigselema02@gmail.com>
2020-06-13 20:06:47 -0500

    Codificación en MainActivity y actBienvenida

--- app/src/main/java/valencia/maps/interfazusuario/MainActivity.java
+++ app/src/main/java/valencia/maps/interfazusuario/MainActivity.java
@@ -2,7 +2,13 @@ package valencia.maps.interfazusuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    public void btnEnviar(View view){
+        //Creamos el Intent
+        Intent intent = new Intent(MainActivity.this, actBienvenida.class);
+        String txtGenero="";
+        RadioButton rbFem = (RadioButton) findViewById(R.id.rbFem);
+        if(rbFem.isChecked())
+            txtGenero="Femenino";
+
+        RadioButton rbMas = (RadioButton) findViewById(R.id.rbMasc);
+        if(rbMas.isChecked())
+            txtGenero="Masculino";
+
+
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        EditText txtFechaN = (EditText)findViewById(R.id.txtFecha);
+        EditText txtTelefono = (EditText)findViewById(R.id.txtTelefono);
+        //Creamos la información a pasar entre actividades - Pares Key-Value
+        Bundle b = new Bundle();
+        b.putString("NOMBRE", txtNombre.getText().toString());
+        b.putString("GENERO", txtGenero);
+        b.putString("FECHA", txtFechaN.getText().toString());
+        b.putString("TELEFONO", txtTelefono.getText().toString());
+        //Añadimos la información al intent
+        intent.putExtras(b);
+
+        //Iniciamos la nueva actividad
+        if(validarVacio(txtNombre,txtGenero,txtFechaN,txtTelefono))
+            startActivity(intent);
+    }
+    public boolean validarVacio(EditText nombre,String genero ,EditText fecha, EditText tel) {
+        boolean band=true;
+        if (nombre.getText().toString().isEmpty())
+        {
+            nombre.setError("No puede quedar vacío");
+            nombre.requestFocus();
+            band=false;
+        }
+        if (genero=="")
+        {
+            Toast.makeText(this, "Seleccione un género", Toast.LENGTH_SHORT).show();
+            band=false;
+        }
+        if (fecha.getText().toString().isEmpty())
+        {
+            fecha.setError("No puede quedar vacío");
+            nombre.requestFocus();
+            band=false;
+        }
+        if (tel.getText().toString().isEmpty())
+        {
+            tel.setError("No puede quedar vacío");
+            nombre.requestFocus();
+            band=false;
+        }
+    return band;
+    }
+
+    }
